e214e067d76b76e1a8ac77f826d90c2950c413d0
Mark Azemoon <mmazemoon@gmail.com>
2015-07-14 13:03:29 -0700

    Enable users to submit links

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ angular.module('mikadoCity', [])
 
     $scope.addPost = function (){
       if(!$scope.title || $scope.title === "") { return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        upvotes: 0,
+        link: $scope.link });
       $scope.title = "";
+      $scope.link = "";
     };
 
     $scope.incrementUpvotes = function(post){
