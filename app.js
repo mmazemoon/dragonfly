97e2ffaafbacadae1f622309c415fa126c090c38
Mark Azemoon <mmazemoon@gmail.com>
2015-07-14 15:04:53 -0700

    Add sample comments to posts object

--- app.js
+++ app.js
@@ -44,7 +44,12 @@ angular.module('mikadoCity', ['ui.router'])
       $scope.posts.push({
         title: $scope.title,
         upvotes: 0,
-        link: $scope.link });
+        link: $scope.link,
+        comments: [
+          {author: "Mark", body: "Cool post!", upvotes:0},
+          {author: "Oliver", body: "Great idea, but everything is wrong!", upvotes: 0}
+        ]
+      });
       $scope.title = "";
       $scope.link = "";
     };
