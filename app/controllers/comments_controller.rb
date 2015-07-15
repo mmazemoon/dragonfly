bb75578179258e146055e881564dd76f79831609
Mark Azemoon <mmazemoon@gmail.com>
2015-07-15 12:41:59 -0700

    Build create and upvote actions for Comments Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      respond_with @post, @comment
+    else
+      respond_with @comment.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def upvote
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.increment!(:upvotes)
+    
+    respond_with @post, @comment
+  end
+
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
