2eef87c6b861564aeff869bb95153a2cd2e9a94f
Mark Azemoon <mmazemoon@gmail.com>
2015-07-14 22:18:29 -0700

    Update templateUrl for home state and posts state

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,12 +8,12 @@ angular.module('mikadoCity', ['ui.router', 'templates'])
     $stateProvider
       .state('home', {
         url: '/home',
-        templateUrl: '/home.html',
+        templateUrl: 'home/_home.html',
         controller: 'MainCtrl'
       })
       .state('posts', {
         url: '/posts/{id}',
-        templateUrl: '/posts.html',
+        templateUrl: 'posts/_posts.html',
         controller: 'PostsCtrl'
       });
 
