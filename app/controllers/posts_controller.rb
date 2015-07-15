a7585e39ddea2682fd57a1eb09a08bc38f7f31fd
Mark Azemoon <mmazemoon@gmail.com>
2015-07-15 12:36:29 -0700

    Create index, show, create, and upvote actions for Posts Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      respond_with @post
+    else
+      respond_with @post.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
+    respond_with @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    respond_with @post
+  end
+
+  def upvote
+    @post = Post.find(params[:id])
+    @post.increment!(:upvotes)
+
+    respond_with @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:link, :title)
+  end
+
 end
