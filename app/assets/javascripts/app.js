8ecc529eb0ec3b83d8aee9bcb42b249da9c5dea8
Mark Azemoon <mmazemoon@gmail.com>
2015-07-14 22:26:14 -0700

    Move PostsCtrl to posts/postsCtrl.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -19,25 +19,4 @@ angular.module('mikadoCity', ['ui.router', 'templates'])
 
       $urlRouterProvider.otherwise('home');
   }
-])
-
-.controller('PostsCtrl', [
-  '$scope',
-  '$stateParams',
-  'posts',
-  function($scope, $stateParams, posts){
-
-    $scope.post = posts.posts[$stateParams.id];
-
-    $scope.addComment = function(){
-      if($scope.body === "") {return;}
-      $scope.post.comments.push({
-        body: $scope.body,
-        author: 'user',
-        upvotes: 0
-      });
-      $scope.body = "";
-    };
-
-  }
 ]);
