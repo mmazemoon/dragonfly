860229819fddd52c8ca49e25f371b4e25d761053
Mark Azemoon <mmazemoon@gmail.com>
2015-07-14 22:24:06 -0700

    Move MainCtrl to home/mainCtrl.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -21,36 +21,6 @@ angular.module('mikadoCity', ['ui.router', 'templates'])
   }
 ])
 
-.controller('MainCtrl', [
-  '$scope',
-  'posts',
-  function($scope, posts){
-
-    $scope.test = "Hello world!";
-
-    $scope.posts = posts.posts;
-
-    $scope.addPost = function (){
-      if(!$scope.title || $scope.title === "") { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        upvotes: 0,
-        link: $scope.link,
-        comments: [
-          {author: "Mark", body: "Cool post!", upvotes:0},
-          {author: "Oliver", body: "Great idea, but everything is wrong!", upvotes: 0}
-        ]
-      });
-      $scope.title = "";
-      $scope.link = "";
-    };
-
-    $scope.incrementUpvotes = function(post){
-      post.upvotes += 1;
-    };
-  }
-])
-
 .controller('PostsCtrl', [
   '$scope',
   '$stateParams',
---
+++ app/assets/javascripts/home/mainCtrl.js
@@ -0,0 +1,30 @@
+angular.module('mikadoCity')
+.controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function($scope, posts){
+
+    $scope.test = "Hello world!";
+
+    $scope.posts = posts.posts;
+
+    $scope.addPost = function (){
+      if(!$scope.title || $scope.title === "") { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        upvotes: 0,
+        link: $scope.link,
+        comments: [
+          {author: "Mark", body: "Cool post!", upvotes:0},
+          {author: "Oliver", body: "Great idea, but everything is wrong!", upvotes: 0}
+        ]
+      });
+      $scope.title = "";
+      $scope.link = "";
+    };
+
+    $scope.incrementUpvotes = function(post){
+      post.upvotes += 1;
+    };
+  }
+]);
