c3faa3461c7f18bf074342bf9eea800654df01a8
Mark Azemoon <mmazemoon@gmail.com>
2015-07-15 13:06:11 -0700

    Use  service to query posts route

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -1,10 +1,19 @@
 angular.module('mikadoCity')
 
 .factory('posts', [
-  function (){
+  '$http',
+  function ($http){
     var o = {
       posts: []
     };
+
+    o.getAll = function(){
+      return $http.get('/posts.json').success(function(data){
+        angular.copy(data, o.posts);
+      });
+    };
+
+
     return o;
   }
 ]);
