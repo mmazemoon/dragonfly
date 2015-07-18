2949aec21852ed654a91d2bc91511223f6ac0893
Mark Azemoon <mmazemoon@gmail.com>
2015-07-17 18:39:12 -0700

    Fix typo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  before_filer :authenticate_user!, only: [:create, :upvote]
+  before_filter :authenticate_user!, only: [:create, :upvote]
 
   def create
     @post = Post.find(params[:post_id])
